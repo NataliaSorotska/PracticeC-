86b9449285a05c3a7ea91f22b2c404be7fdb7944
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-09-18 22:18:29 +0300

    Practice Inheritance

---
+++ PracticeCSharp/PracticeCSharp/BaseClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp
+{
+    class BaseClass
+    {
+        public String publicField = "BaseClass.PublicField";
+        private String privateField = "BaseClass.PrivateField";
+        protected String protectedField = "BaseClass.ProtectedField";
+
+        public void Show()
+        {
+            Console.WriteLine(privateField);
+        }
+    }
+}
