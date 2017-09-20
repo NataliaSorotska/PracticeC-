4b8a1e5d5d7cf0e366e98ac1f44fb0f2272a21b0
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-09-20 17:37:19 +0300

    Practice Buble Sort part 2

--- PracticeCSharp/PracticeCSharp/Program.cs
+++ PracticeCSharp/PracticeCSharp/Program.cs
@@ -15,33 +15,39 @@ namespace PracticeCSharp
             Program program = new Program();
             int[] array = new[] { 1, 8, 5, 3, 9 };
 
-            int count = 0;
-            int left;
-            int right;
+            //int count = 0;
+            //int left;
+            //int right;
 
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                left = array[i];
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    count++;
-                    right = array[j];
-                    if (right > left)
-                    {
-                        program.Swap(array, i, j);
-                    }
-                }
+            //for (int i = 0; i < array.Length - 1; i++)
+            //{
+            //    left = array[i];
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        count++;
+            //        right = array[j];
+            //        if (right > left)
+            //        {
+            //            program.Swap(array, i, j);
+            //        }
+            //    }
 
-            }
+            //}
 
 
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{array[i]} ");
+            //}
+
+            //Console.WriteLine();
+            //Console.Write($"count {count}");
+
+            program.Sort(array);
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write($"{array[i]} ");
             }
-
-            Console.WriteLine();
-            Console.Write($"count {count}");
 
             Console.ReadKey();
         }
@@ -58,7 +64,23 @@ namespace PracticeCSharp
                 items[right] = items[right] ^ items[left];
             }
         }
+        public void Sort(int [] items)
+        {
+            bool swapped;
 
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < items.Length; i++)
+                {
+                    if (items[i - 1].CompareTo(items[i]) > 0)
+                    {
+                        Swap(items, i - 1, i);
+                        swapped = true;
+                    }
+                }
+            } while (swapped != false);
+        }
     }
 }
 
